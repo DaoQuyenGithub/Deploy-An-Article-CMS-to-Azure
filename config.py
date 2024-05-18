d3e58a01949da8cd511022fe9c183718f17305d3
Sofia <dquyen2806@gmail.com>
2024-05-18 16:40:42 +0700

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsudacity'
     BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'VGa4uPwlqELSUlynZaLCYxEqkYFM/PS461VK7BiygfuoOCKPPqLuGnJHaZnmm8Pp7kDYvq68LITe+AStNbNvgg=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'image'
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
 
     SQL_SERVER = os.environ.get('SQL_SERVER') or 'cms-database.database.windows.net'
     SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'cms'
